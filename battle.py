976f2a3d568f7494cc06440ac9512dd6b0616436
Emmanuel <e.rosa@estudiantes.matcom.uh.cu>
2019-05-02 21:35:50 -0400

    todavia en la batalla

--- battle.py
+++ battle.py
@@ -1,11 +1,27 @@
 from castle import *
 import threading as thr
 from hero import Hero
+from datetime import datetime, timedelta
+from time import sleep
 
 class Battle:
     def __init__(self):
         self.run_battle_thread = thr.Thread(target= self.run_battle)
         self.run_battle_thread.start()
+
+    def run_battle(self):
+        work_in_tree = False
+        now = datetime.now()
+
+        if now.hour == 11 and now.minute >= 30 and now.minute < 50 and not work_in_tree:
+            work_in_tree = True
+            self.tree_construction()
+        
+        if now.hour == 11 and now.minute >= 50 and not chose_atack:
+            chose_atack = True
+            self.sharing_tree()
+
+        sleep(10)
 
     def tree_construction(self):
         threads = []
@@ -15,3 +31,5 @@ class Battle:
                 # hero.in_quest = "Armando arbol de defensa"
                 threads.append(thr.Thread(target=hero.put_node))
                 threads[-1].start()
+
+    def sharing_tree(self):
