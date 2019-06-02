bfc956b6ce692561aeded2e25dde5b88cb0c6cd6
Emmanuel <e.rosa@estudiantes.matcom.uh.cu>
2019-06-02 07:28:35 -0400

    recuperado

---
+++ initialize.py
@@ -0,0 +1,11 @@
+import telebot
+# from emoji import emojize
+import threading as thr
+
+bot = telebot.TeleBot("878916725:AAGpCSgEJB3UvmEUexOkSPmuIWiJcnBenJ0")
+
+print('Starting!')
+
+users = {}
+
+# tree_emoji = emojize(":evergreen_tree:", use_aliases=True)
